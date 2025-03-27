7438d2566ded68350bc1920d226ac78ead9a8017
Igor Cedro <igor9cedro@gmail.com>
2025-03-27 15:01:35 -0300

    Criado o projeto Ionic Angular

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tracker.pet',
+  webDir: 'www'
+};
+
+export default config;
